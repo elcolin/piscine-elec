ce03c7f44d52621491f0fefb38da1459debfa8cb
ecolin <ecolin@student.42.fr>
2024-04-15 14:53:26 +0200

    day over

--- ex04/main.c
+++ ex04/main.c
@@ -1,36 +1,66 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_ON PORTB |= (1 << PB0)
-#define LED_OFF PORTB &= ~(1 << PB0)
-#define BUTTON_PRESSED !(PIND & (1 << PD2))
+// #define LED_ON PORTB |= (1 << PB0)
+// #define LED_OFF PORTB &= ~(1 << PB0)
+#define SW1_PRESSED !(PIND & (1 << PD2))
+#define SW1_UNPRESSED (PIND & (1 << PD2))
+#define SW2_PRESSED !(PIND & (1 << PD4))
+#define SW2_UNPRESSED (PIND & (1 << PD4))
 //LED is on port PB0
+
+void binary_to_led(int value)
+{
+    char PBs[4] = {PB0, PB1, PB2, PB4};
+    int i = 3;
+    while (i >= 0)
+    {
+        if (1 & (value >> i))
+        // shifts the bits on a 4 bit range: 1000 >> 3 gives 0001 and we do the & operation
+        // if it gives 1, then we enter the condition to turn on the LED
+            PORTB |= (1 << PBs[i]);
+        else 
+            PORTB &= ~(1 << PBs[i]);
+        i--;
+    }
+}
 
 
 int main()
 {
+    int value = 0;
     //DDRB because we use brooch 0 on B port (PBO)
-    int power = 0;
-     DDRB |= (1<< PB0);
-    //PB0 is set as output
+    DDRB |= (1 << PB0);
+    DDRB |= (1 << PB1);
+    DDRB |= (1 << PB2);
+    DDRB |= (1 << PB4);
+    //LED D1, D2, D3 and D4 set as output
+
     DDRB &= ~(1 << PD2);
-    //PD2 is set as input
+    DDRB &= ~(1 << PD4);
+    //PD2 and PD4 is set as input
     while (1)
     {
-        if (BUTTON_PRESSED)
+        if (SW1_PRESSED)
         {
             _delay_ms(100);
             //delay to avoid debouncing effect
-            if ((PIND & (1 << PD2)))
+            if (SW1_UNPRESSED)
+                value++;
+            binary_to_led(value);
             //checks if button came back to original position
-                power = !power;
-            if (power)
-                LED_ON;
-            else
-                LED_OFF;
-            
         }
-    }
+        if (SW2_PRESSED)
+        {
+            _delay_ms(100);
+            //delay to avoid debouncing effect
+            if (SW2_UNPRESSED && value)
+                value--;
+            binary_to_led(value);
+            //checks if button came back to original position
+        }
 
+        
+    }
    return 0; 
 }
